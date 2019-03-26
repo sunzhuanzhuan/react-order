3e3a9d0603f66aab255fc5735d52553323cfef00
lzb <lzbabd@gmail.com>
2019-03-26 10:36:19 +0800

    完善linkTo函数

--- src/util/linkTo.js
+++ src/util/linkTo.js
@@ -6,8 +6,21 @@ export const linkTo = (href = '/', type = 'replace') => {
     href = '/' + href
   }
   if (process.env.NODE_ENV === 'development') {
-    window.location[type](process.env.REACT_APP_TRUNK_BENTLEY_ADDRESS + href);
+    href = process.env.REACT_APP_TRUNK_BENTLEY_ADDRESS + href
+  }
+  if (type === 'push') {
+    window.location.href = href;
+  } else if (type === '_blank') {
+    let a = document.createElement('a');
+    a.href = href
+    a.target = type
+    let e = document.createEvent('MouseEvents');
+    e.initEvent('click', true, true);
+    setTimeout(() => {
+      a.dispatchEvent(e)
+    }, 500);
   } else {
     window.location[type](href);
   }
+  return null
 }
