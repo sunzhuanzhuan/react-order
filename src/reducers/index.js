b1bae35e082af111ee18f0c618d246a2e6628af9
sunzhuanzhuan <sunzhuanzhuan@weiboyi.com>
2019-05-05 14:10:02 +0800

    去掉不必要reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,7 +6,6 @@ import loginReducer from '../login/reducer/index'
 import siderMenuReducer from '../siderMenu/reducers'
 import authorizationsReducers from './authorizations'
 import trinityReconciliationsReducers from '../trinityReconciliations/reducer';
-import exportTemplateReducer from '../components/exportTemplate/reducer';
 import statement from '../trinityReconciliations/reducer'
 export default combineReducers({
 	commonReducers,
@@ -16,6 +15,5 @@ export default combineReducers({
 	siderMenuReducer,
 	authorizationsReducers,
   trinityReconciliationsReducers,
-  exportTemplateReducer,
   statement
 });
