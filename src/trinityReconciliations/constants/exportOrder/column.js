760b4c2a509b5225ad3df0d3853e23e7a1020f33
sunzhuanzhuan <sunzhuanzhuan@weiboyi.com>
2019-05-28 18:37:22 +0800

    -save

--- src/trinityReconciliations/constants/exportOrder/column.js
+++ src/trinityReconciliations/constants/exportOrder/column.js
@@ -572,7 +572,6 @@ export const summaryTotalDetailListFunc = () => {
       align: 'center',
       width: 100,
       render: (text, record) => {
-        console.log(record)
         return record.operation_type == 4 ? parseFloat(record.pay_amount).toFixed(2) : '-'
       }
     },
