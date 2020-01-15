22ba0282756b40a74612fed75353dea90d7f1fcf
sunzhuanzhuan <sunzhuanzhuan@weiboyi.com>
2020-01-15 18:48:50 +0800

    筛选项增加filterOPtions

--- src/taskPool/components/Setting/Notice.js
+++ src/taskPool/components/Setting/Notice.js
@@ -8,6 +8,7 @@ const Notice = (props) => {
   const [fetching, setFetching] = useState(false)
   const [userData, setUserData] = useState([])
   const [selectedUser, setSelectedUser] = useState([])
+  const [value, setValue] = useState([])
   let fetchData = (value) => {
     if (!value) {
       return
@@ -20,28 +21,28 @@ const Notice = (props) => {
         pageSize: 30
       },
       form: {
-        userName: user_info.username,
-        realName: value
+        userName: value
       }
     }
     setFetching(true)
+    setUserData([])
     TPQueryUserInfo(search).then((res) => {
       setFetching(false)
       setUserData(res.data.list)
     })
   }
-  fetchData = debounce(fetchData, 800);
+  fetchData = debounce(fetchData, 1000);
   const handleDelete = (record) => {
     confirm({
       title: '删除人员',
-      content: `确定要在通知列表中${record.personnelName}删除么？`,
+      content: `确定要在通知列表中${record.realName}删除么？`,
       okText: '确定',
       okType: 'danger',
       cancelText: '取消',
       onOk() {
         let params = {
-          userId: record.id,
-          isDelete: 1,
+          userIds: [record.userId],
+          isDeleted: 1,
           notificationType: 11,
           platformId: 9
         }
@@ -76,23 +77,23 @@ const Notice = (props) => {
     },
     {
       title: '姓名',
-      dataIndex: 'personnelName',
+      dataIndex: 'realName',
       align: 'center',
       width: 100,
-      key: 'personnelName',
+      key: 'realName',
     }, {
       title: '岗位名称',
-      dataIndex: 'jobType',
+      dataIndex: 'jobTypes',
       align: 'center',
       width: 100,
-      key: 'jobType',
+      key: 'jobTypes',
     },
     {
       title: '手机号',
-      dataIndex: 'mobile',
+      dataIndex: 'cellPhone',
       align: 'center',
       width: 100,
-      key: 'mobile',
+      key: 'cellPhone',
     },
     {
       title: '邮箱',
@@ -114,9 +115,17 @@ const Notice = (props) => {
   ];
   const handleChange = (value) => {
     setSelectedUser(value)
+    setFetching(false)
+    setValue(value)
   }
   const handleApply = () => {
-    props.TPUpdateNotice(selectedUser).then(() => {
+    let config = {
+      userIds: selectedUser,
+      notificationType: 11,
+      platformId: 9
+    }
+    props.TPUpdateNotice(config).then(() => {
+      setValue([])
       let params = {
         page: {
           currentPage: 1,
@@ -129,12 +138,11 @@ const Notice = (props) => {
       }
       props.TPGetNotificationList(params)
     })
-    console.log(selectedUser);
+
 
   }
   const { list = [], } = props.notificationList;
   const { list: searchList = [] } = props.tpUserInfo
-
   return (
     <div>
       <h2 style={{ marginTop: '20px' }}>微信公众号</h2>
@@ -145,9 +153,11 @@ const Notice = (props) => {
             <Select
               style={{ width: '500px' }}
               mode="multiple"
-              notFoundContent={fetching ? <Spin size="small" /> : null}
+              value={value}
+              notFoundContent={fetching ? <Spin size="small" /> : '暂无数据'}
               placeholder="请搜索需要通知的人员"
               onSearch={fetchData}
+              filterOption={false}
               onChange={handleChange}
             >
               {userData.map((item, index) => {
