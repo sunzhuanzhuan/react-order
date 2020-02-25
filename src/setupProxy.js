62941dcd51c381ca64aedfc2f4c820fc87fb2b31
wangxinyue <wangxinyue@weiboyi>
2020-02-25 20:34:23 +0800

    调整领取列表分页取值

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -27,6 +27,6 @@ module.exports = function (app) {
   // ));
   // http://easymock.wby.me:7300/mock/5c874e0b73f3100017a641c1/api/summaryData
   app.use(proxy('/api',
-    { target: 'http://nb.pre-weiboyi.com', changeOrigin: true }
+    { target: 'http://nb.weiboyi.com', changeOrigin: true }
   ));
 }
