1f71ee11d253d2f48f315ceba093df94691f767a
baikeqi <baikeqii@163.com>
2020-05-27 09:39:37 +0800

    doconflict

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,32 +1,14 @@
 /*
- * @Descripttion: 
+ * @Descripttion:
  * @Author: wangxinyue
  * @Date: 2020-02-24 13:24:22
  */
 const proxy = require('http-proxy-middleware');
 module.exports = function (app) {
-  // app.use(proxy('/api/spotplan/',
-  //   { target: 'http://192.168.100.117:30010', changeOrigin: true }
-  // ));
-
-  app.use(proxy('/api/operator-gateway/selectUserIdBySnsNameOrSnsId',
-    { target: 'http://yapi.ops.tst-weiboyi.com/mock/129', changeOrigin: true }
-  ));
-  // app.use(proxy('/api/operator-gateway/adOrder/v1/trip/priceCalculation',
-  //   { target: 'http://yapi.ops.tst-weiboyi.com/mock/129', changeOrigin: true }
-  // ));
-  // app.use(proxy('/api/operator-gateway/adOrder/v1/weixin/priceCalculation',
-  //   { target: 'http://yapi.ops.tst-weiboyi.com/mock/129', changeOrigin: true }
-  // ));
-  // app.use(proxy('/api/operator-gateway/adOrder/v1/detail_2',
-  //   { target: 'http://yapi.ops.tst-weiboyi.com/mock/129', changeOrigin: true }
-  // ));
-
-  // app.use(proxy('/api/operator-gateway/',
-  //   { target: 'http://yapi.ops.tst-weiboyi.com/mock/129', changeOrigin: true }
-  // ));
-  // http://easymock.wby.me:7300/mock/5c874e0b73f3100017a641c1/api/summaryData
+  /*app.use(proxy('/api/summaryData/getPlatformDataInfo',
+    { target: 'http://yapi.ops.tst-weiboyi.com/mock/257', changeOrigin: true }
+  ));*/
   app.use(proxy('/api',
-    { target: 'http://nb.dev-weiboyi.com', changeOrigin: true }
+    { target: process.env.REACT_APP_TRUNK_BENTLEY_ADDRESS, changeOrigin: true }
   ));
 }
